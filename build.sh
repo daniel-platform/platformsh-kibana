516db5f87803b86faea08685e8cacab8815b26ad
Daniel Morante <daniel@platform.sh>
2018-04-05 00:48:33 -0400

    Retry

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ KIBANA_DL_ARCHIVE="kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz"
 mkdir -p ${PLATFORM_APP_DIR}/kibana;
 
 # Download and Extract Kibana
+echo "Downloading"
 tar xzv -C ${PLATFORM_APP_DIR}/kibana --strip 1 < <(wget --no-cookies --no-check-certificate -q -O - ${KIBANA_DOWNLOAD_URI}/${KIBANA_DL_ARCHIVE})
 
 # Apply Platform.sh Patches
