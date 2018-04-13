f32bfabf4fb9740085ea356f7652bf7851a44a62
Daniel Morante <daniel@platform.sh>
2018-04-13 12:50:08 -0400

    Move some stuff around, enable verbose logging

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,4 @@
 # Load header
 . ./header.inc
 
-# Extract database configuration from enviroment
-#export KIBANA_ES_PORT=$(bin/json_env PLATFORM_RELATIONSHIPS billinges.elasticsearch.port)
-#export KIBANA_ES_HOST=$(bin/json_env PLATFORM_RELATIONSHIPS billinges.elasticsearch.host)
-#export KIBANA_ES_SCHEME=$(bin/json_env PLATFORM_RELATIONSHIPS billinges.elasticsearch.scheme)
-export KIBANA_ES_PORT=$(bin/json_env PLATFORM_RELATIONSHIPS billinges.billinges.port)
-export KIBANA_ES_HOST=$(bin/json_env PLATFORM_RELATIONSHIPS billinges.billinges.host)
-export KIBANA_ES_SCHEME=$(bin/json_env PLATFORM_RELATIONSHIPS billinges.billinges.scheme)
-export KIBANA_ES_URL="${KIBANA_ES_SCHEME}://${KIBANA_ES_HOST}:${KIBANA_ES_PORT}"
-
-echo "Generating Configuration..."
-
-# Generate a databse configuration
-cat << EOF > ${PLATFORM_APP_DIR}/config/kibana_config.yml
-elasticsearch.url: ${KIBANA_ES_URL}
-path.data: ${PLATFORM_APP_DIR}/data
-pid.file: ${PLATFORM_APP_DIR}/temp/kibana.pid
-server.port: ${PORT}
-EOF
 
